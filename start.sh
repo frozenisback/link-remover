7c9c7fab03703dd35b1bef14044cd5d76a8a357c
frozenisback <k83090240@gmail.com>
2025-11-14 20:15:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 echo "Starting Telethon Link Remover Bot..."
-python3 mastlinkbot.py
+python3 mastlinkbot.py &
+
+echo "Starting  HTTP Server..."
+python3 standalone_server.py &
+
+# keep container alive
+wait
+
